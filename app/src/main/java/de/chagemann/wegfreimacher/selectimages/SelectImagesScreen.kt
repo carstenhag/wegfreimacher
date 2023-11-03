3cb1c5c991406a27664e02a1a9cd9cb33cff0400
Carsten Hagemann <carsten@chagemann.de>
2023-11-03 21:26:20 +0100

    work on SelectImagesScreen

--- app/src/main/java/de/chagemann/wegfreimacher/selectimages/SelectImagesScreen.kt
+++ app/src/main/java/de/chagemann/wegfreimacher/selectimages/SelectImagesScreen.kt
@@ -1,25 +1,70 @@
 package de.chagemann.wegfreimacher.selectimages
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import de.chagemann.wegfreimacher.ui.theme.WegfreimacherTheme
 
 @Composable
 fun SelectImagesScreen(
-    viewModel: SelectImagesViewModel = hiltViewModel()
+    viewModel: SelectImagesViewModel = hiltViewModel(),
+    onSelectImagesClicked: () -> Unit
 ) {
-    SelectImagesContent()
+    SelectImagesContent(onSelectImagesClicked = onSelectImagesClicked)
 }
 
 @Composable
-fun SelectImagesContent() {
-    Column(modifier = Modifier.fillMaxSize()) {
-        Text(text = "adadssasd")
+fun SelectImagesContent(
+    modifier: Modifier = Modifier,
+    onSelectImagesClicked: () -> Unit
+) {
+    Column(modifier = modifier.fillMaxSize()) {
+        NoticeCreationSteps(modifier = Modifier.align(Alignment.CenterHorizontally))
+        Spacer(modifier = Modifier.height(72.dp))
+        Button(
+            onClick = onSelectImagesClicked,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text(text = "Select images")
+        }
+    }
+}
+
+@Composable
+private fun NoticeCreationSteps(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .padding(horizontal = 16.dp)
+            .padding(top = 16.dp)
+            .background(
+                Color.LightGray,
+                shape = MaterialTheme.shapes.small
+            )
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Schritte zur Erstellung einer Meldung:",
+            modifier = Modifier.padding(bottom = 8.dp),
+            style = MaterialTheme.typography.titleMedium
+        )
+        Text(text = "1.\t\tFotos auswählen & hochladen")
+        Text(text = "2.\t\tTatbestand auswählen, Daten ausfüllen")
+        Text(text = "3.\t\tMeldung überprüfen und einreichen")
     }
 }
 
@@ -28,6 +73,8 @@ fun SelectImagesContent() {
 @Composable
 fun SelectImagesContentPreview() {
     WegfreimacherTheme {
-        SelectImagesContent()
+        SelectImagesContent(
+            onSelectImagesClicked = {}
+        )
     }
 }
