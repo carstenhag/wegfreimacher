0ef4eb2a18b4c3a728a9c26048450e999521d739
Carsten Hagemann <carsten@chagemann.de>
2024-04-03 23:34:14 +0200

    update kotlin compiler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ android {
     }
     composeOptions {
         // See https://developer.android.com/jetpack/androidx/releases/compose-kotlin
-        kotlinCompilerExtensionVersion = "1.5.10"
+        kotlinCompilerExtensionVersion = "1.5.11"
     }
     packaging {
         resources {
